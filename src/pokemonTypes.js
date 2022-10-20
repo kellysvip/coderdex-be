cc1bc0077b8d31238c69dfa1a90575b9f33a7dfa
Kelly <20522051@gm.uit.edu.vn>
2022-10-21 02:13:34 +0700

    add file

---
+++ src/pokemonTypes.js
@@ -0,0 +1,2 @@
+ const pokemonTypes = ['bug', 'dragon', 'fairy', 'fire', 'ghost', 'ground', 'normal', 'psychic', 'steel', 'dark', 'electric', 'fighting', 'flying', 'grass', 'ice', 'poison', 'rock', 'water'];
+ module.exports = {pokemonTypes}
